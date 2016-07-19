ca2b4a48e18860665227c85e410b08c745da2311
java <java>
2016-07-19 16:21:07 +0900

    label Hello add

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -5,7 +5,7 @@ public class MainApplication {
 	public static void main(String[] args) {
 		
 		System.out.println("###################");
-		System.out.println("Hello First gradle");
+		System.out.println("Hello First gradle..!");
 		System.out.println("###################");
 	
 
